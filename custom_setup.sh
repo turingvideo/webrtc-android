26345e9888f3290c261ac9a2d1a69623489470fc
cnwangxiao <wangxiao@turingvideo.net>
2023-02-08 16:37:10 +0800

    add jitpack.yml

---
+++ custom_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+VERSION=104.5112.06.3
+
+RELEASE_VERSION=${VERSION}
+
+SDK_BIN_URL=https://github.com/turingvideo/webrtc-android/releases/download/${VERSION}/libwebrtc.aar
+
+BUILD_DIR=build
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+echo "Downloading webrtc-sdk ${VERSION} binary for android."
+curl -L -O ${SDK_BIN_URL}
+
+mvn install:install-file \
+    -Dfile=libwebrtc.aar \
+    -Dpackaging=aar \
+    -Dversion=${RELEASE_VERSION} \
+    -DgroupId=com.github.turingvideo \
+    -DartifactId=android
